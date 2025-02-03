4ca12b206aaa33abc99ca02e2d27474532e0f276
Cesar Augusto <64844107+cesaraugustodev2@users.noreply.github.com>
2025-02-03 16:19:04 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from functions import load_phonebook, save_phonebook, add_entry, view_phonebook,main_menu
+
+def main():
+    filename = 'phonebook.json'
+    phonebook = load_phonebook(filename)
+    main_menu(phonebook,filename)
+
+
+if __name__ == '__main__':
+    main()
