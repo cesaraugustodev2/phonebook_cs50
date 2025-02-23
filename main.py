dbdb731575bbd5013ee145ad34b04622b545b932
Cesar Augusto <64844107+cesaraugustodev2@users.noreply.github.com>
2025-02-23 21:42:12 +0000

    Sun, Feb 23, 2025, 6:42 PM -03:00

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from functions import load_phonebook, save_phonebook, add_entry, view_phonebook,main_menu
 
+
 def main():
     filename = 'phonebook.json'
     phonebook = load_phonebook(filename)
     main_menu(phonebook,filename)
+    other_function()
+
+
 
 
 if __name__ == '__main__':
---
+++ main.test.py
@@ -0,0 +1,8 @@
+import unittest
+from main import other_function
+
+class TestOtherFunction(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
